49bdc70a22ba4f00f7838ddd2dbad0e8cc7cb6f2
CodyEdwardsDurham <37095079+CodyEdwardsDurham@users.noreply.github.com>
2018-03-20 13:24:16 -0400

    Lab 3 For .Net Development
    Lab 3 For .Net Development

---
+++ frmAverageUnitsShipped.Designer.vb
@@ -0,0 +1,213 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmAverageUnitsShipped
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.lbUnits = New System.Windows.Forms.Label()
+        Me.tbUnits = New System.Windows.Forms.TextBox()
+        Me.lbDay = New System.Windows.Forms.Label()
+        Me.btnEnter = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lbAnswer = New System.Windows.Forms.Label()
+        Me.tbList = New System.Windows.Forms.TextBox()
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        Me.lbAnswer2 = New System.Windows.Forms.Label()
+        Me.tbList2 = New System.Windows.Forms.TextBox()
+        Me.lbAnswer3 = New System.Windows.Forms.Label()
+        Me.tbList3 = New System.Windows.Forms.TextBox()
+        Me.lbAnswerTotal = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lbUnits
+        '
+        Me.lbUnits.Location = New System.Drawing.Point(41, 55)
+        Me.lbUnits.Name = "lbUnits"
+        Me.lbUnits.Size = New System.Drawing.Size(31, 13)
+        Me.lbUnits.TabIndex = 0
+        Me.lbUnits.Text = "&Units"
+        Me.lbUnits.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'tbUnits
+        '
+        Me.tbUnits.Location = New System.Drawing.Point(86, 55)
+        Me.tbUnits.Name = "tbUnits"
+        Me.tbUnits.ShortcutsEnabled = False
+        Me.tbUnits.Size = New System.Drawing.Size(100, 20)
+        Me.tbUnits.TabIndex = 0
+        Me.ToolTip1.SetToolTip(Me.tbUnits, "Enter information here")
+        '
+        'lbDay
+        '
+        Me.lbDay.AutoSize = True
+        Me.lbDay.Location = New System.Drawing.Point(41, 23)
+        Me.lbDay.Name = "lbDay"
+        Me.lbDay.Size = New System.Drawing.Size(38, 13)
+        Me.lbDay.TabIndex = 2
+        Me.lbDay.Text = "Day: 0"
+        '
+        'btnEnter
+        '
+        Me.btnEnter.Location = New System.Drawing.Point(111, 269)
+        Me.btnEnter.Name = "btnEnter"
+        Me.btnEnter.Size = New System.Drawing.Size(75, 23)
+        Me.btnEnter.TabIndex = 1
+        Me.btnEnter.Text = "&Enter"
+        Me.ToolTip1.SetToolTip(Me.btnEnter, "Enter Number Into Calculation")
+        Me.btnEnter.UseVisualStyleBackColor = True
+        '
+        'btnReset
+        '
+        Me.btnReset.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnReset.Location = New System.Drawing.Point(192, 269)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(75, 23)
+        Me.btnReset.TabIndex = 2
+        Me.btnReset.Text = "Reset"
+        Me.ToolTip1.SetToolTip(Me.btnReset, "Reset Program ")
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnExit.Location = New System.Drawing.Point(273, 269)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 3
+        Me.btnExit.Text = "E&xit"
+        Me.ToolTip1.SetToolTip(Me.btnExit, "Exit the Program")
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lbAnswer
+        '
+        Me.lbAnswer.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lbAnswer.Location = New System.Drawing.Point(38, 200)
+        Me.lbAnswer.Name = "lbAnswer"
+        Me.lbAnswer.Size = New System.Drawing.Size(123, 27)
+        Me.lbAnswer.TabIndex = 6
+        Me.lbAnswer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.ToolTip1.SetToolTip(Me.lbAnswer, "Answer Box")
+        '
+        'tbList
+        '
+        Me.tbList.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.tbList.Location = New System.Drawing.Point(38, 94)
+        Me.tbList.Multiline = True
+        Me.tbList.Name = "tbList"
+        Me.tbList.Size = New System.Drawing.Size(123, 103)
+        Me.tbList.TabIndex = 80
+        Me.tbList.TabStop = False
+        Me.ToolTip1.SetToolTip(Me.tbList, "List of Entrys ")
+        '
+        'lbAnswer2
+        '
+        Me.lbAnswer2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lbAnswer2.Location = New System.Drawing.Point(167, 200)
+        Me.lbAnswer2.Name = "lbAnswer2"
+        Me.lbAnswer2.Size = New System.Drawing.Size(123, 27)
+        Me.lbAnswer2.TabIndex = 6
+        Me.lbAnswer2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'tbList2
+        '
+        Me.tbList2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.tbList2.Location = New System.Drawing.Point(167, 94)
+        Me.tbList2.Multiline = True
+        Me.tbList2.Name = "tbList2"
+        Me.tbList2.Size = New System.Drawing.Size(123, 103)
+        Me.tbList2.TabIndex = 80
+        Me.tbList2.TabStop = False
+        '
+        'lbAnswer3
+        '
+        Me.lbAnswer3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lbAnswer3.Location = New System.Drawing.Point(301, 200)
+        Me.lbAnswer3.Name = "lbAnswer3"
+        Me.lbAnswer3.Size = New System.Drawing.Size(123, 27)
+        Me.lbAnswer3.TabIndex = 6
+        Me.lbAnswer3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'tbList3
+        '
+        Me.tbList3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.tbList3.Location = New System.Drawing.Point(301, 94)
+        Me.tbList3.Multiline = True
+        Me.tbList3.Name = "tbList3"
+        Me.tbList3.Size = New System.Drawing.Size(123, 103)
+        Me.tbList3.TabIndex = 80
+        Me.tbList3.TabStop = False
+        '
+        'lbAnswerTotal
+        '
+        Me.lbAnswerTotal.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lbAnswerTotal.Location = New System.Drawing.Point(38, 239)
+        Me.lbAnswerTotal.Name = "lbAnswerTotal"
+        Me.lbAnswerTotal.Size = New System.Drawing.Size(386, 27)
+        Me.lbAnswerTotal.TabIndex = 6
+        Me.lbAnswerTotal.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'frmAverageUnitsShipped
+        '
+        Me.AcceptButton = Me.btnEnter
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnReset
+        Me.ClientSize = New System.Drawing.Size(452, 304)
+        Me.Controls.Add(Me.tbList3)
+        Me.Controls.Add(Me.lbAnswer3)
+        Me.Controls.Add(Me.tbList2)
+        Me.Controls.Add(Me.lbAnswerTotal)
+        Me.Controls.Add(Me.lbAnswer2)
+        Me.Controls.Add(Me.tbList)
+        Me.Controls.Add(Me.lbAnswer)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.btnEnter)
+        Me.Controls.Add(Me.lbDay)
+        Me.Controls.Add(Me.tbUnits)
+        Me.Controls.Add(Me.lbUnits)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmAverageUnitsShipped"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Average Units Shipped"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lbUnits As Label
+    Friend WithEvents tbUnits As TextBox
+    Friend WithEvents lbDay As Label
+    Friend WithEvents btnEnter As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents lbAnswer As Label
+    Friend WithEvents tbList As TextBox
+    Friend WithEvents ToolTip1 As ToolTip
+    Friend WithEvents lbAnswer2 As Label
+    Friend WithEvents tbList2 As TextBox
+    Friend WithEvents lbAnswer3 As Label
+    Friend WithEvents tbList3 As TextBox
+    Friend WithEvents lbAnswerTotal As Label
+End Class
---
+++ frmAverageUnitsShipped.vb
@@ -0,0 +1,97 @@
+﻿Option Strict On
+''' Title:  Average Units Shiped by Employee
+''' Author: Cody Edwards 100666080
+''' Since:  March 1st
+''' Description: Program to Calulcate the average units shipped at the end of every week
+''' CRN: NETD-2202-04
+''' 
+Public Class frmAverageUnitsShipped
+    Dim employeeCounter As Integer
+    Dim dayCounter As Integer = 0 ' dayCounter varaiable 
+    Dim listArray(7, 2) As Integer ' array to store each days sales 
+    Private Sub btnEnter_Click(sender As Object, e As EventArgs) Handles btnEnter.Click
+        Calculate(employeeCounter)
+    End Sub
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Application.Exit() 'exits the program when the exit button is clicked 
+    End Sub
+
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        Reset()
+    End Sub
+    Private Sub Reset()
+        employeeCounter = 0
+        dayCounter = 0 'resets all variables and text boxes if the reset button is clicked 
+        tbList.Clear()
+        tbList2.Clear()
+        tbList3.Clear()
+        tbUnits.Clear()
+        lbDay.Text = "Day: "
+        tbUnits.Clear()
+        tbList.Enabled = True
+        tbList2.Enabled = True
+        tbList3.Enabled = True
+        tbUnits.Enabled = True
+        btnEnter.Enabled = True
+        lbAnswer.Text = ""
+        lbAnswer2.Text = ""
+        lbAnswer3.Text = ""
+        lbAnswerTotal.Text = ""
+        tbUnits.Focus()
+    End Sub
+    Private Sub Disable()
+        employeeCounter = 0 'disableing all the buttons and reseting the rounder 
+        tbList2.Enabled = False
+        tbList3.Enabled = False
+        tbList.Enabled = False
+        tbUnits.Enabled = False
+        btnEnter.Enabled = False
+    End Sub
+    Function Calculate(arrayPosition As Integer) As Integer
+        Static totalAverage As Single
+        Static textboxList(2) As TextBox
+        textboxList(0) = tbList
+        textboxList(1) = tbList2
+        textboxList(2) = tbList3
+        Static lableList(2) As Label
+        lableList(0) = lbAnswer
+        lableList(1) = lbAnswer2
+        lableList(2) = lbAnswer3
+        Dim isNum As Integer 'temp variable for the tryparse method 
+        Dim average As Single 'average variable to show and hold the average number of sales 
+        If Int32.TryParse(tbUnits.Text, isNum) Then 'Makes sure that user input is a Whole Number
+            If CInt(tbUnits.Text) < 1000 And CInt(tbUnits.Text) > 0 Then 'checks if the user input is between 0 and 1000
+                listArray(dayCounter, arrayPosition) = CInt(tbUnits.Text) 'stores that input in the array
+                textboxList(arrayPosition).Text += (listArray(dayCounter, arrayPosition).ToString) + Environment.NewLine 'outputs the array to the text box 
+                dayCounter = dayCounter + 1 'increment the dayCounter 
+                lbDay.Text = "Day: " + dayCounter.ToString 'move to the next day
+                tbUnits.Clear() 'clears the input text box 
+            Else
+                MessageBox.Show("Between 0 and 1000 inclusive") 'if it wasnt between 0 and 1000 show error
+                tbUnits.Clear()
+            End If
+        Else
+            MessageBox.Show("Numeric Entrys Only") ' if it wasnt numeric show error 
+            tbUnits.Clear()
+        End If
+
+        If dayCounter >= 7 Then 'checks what day it is and if its day 7 it will start calculating thee average 
+            For index As Integer = 0 To 6 'for loop to calculate the average 
+                average += listArray(index, arrayPosition)
+            Next
+            totalAverage += average
+            average = average / 7
+            lableList(arrayPosition).Text = "Average: " + Math.Round(average, 2).ToString 'showing the answer in the answer label and also rounding it to 2 decimals 
+            If arrayPosition >= 2 And dayCounter >= 7 Then
+                dayCounter = 0
+                Disable()
+                lbAnswerTotal.Text = "Average per day: " + Math.Round((totalAverage / 3), 2).ToString
+                totalAverage = 0
+            Else
+                dayCounter = 0
+                employeeCounter += 1
+            End If
+        End If
+        Return 1
+    End Function
+End Class
